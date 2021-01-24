2547bad1937923db32cb62948eaad2f06c5aa407
Bojan Nikolic <bn204@cam.ac.uk>
2021-01-24 13:11:26 +0000

    Refactor, opt check convergence message

--- brutus/__init__.py
+++ brutus/__init__.py
@@ -7,30 +7,45 @@ from jax.scipy.optimize import minimize as jminimize
 import scipy.optimize
 
 _lfns={"Gauss" : lambda delta, w: -1*(delta**2/w**2).sum(),
+       "Cauchy": lambda delta, w: numpy.log(w**2/(w**2+delta**2)).sum()
        }
+
+def mkll(Y, mm, w, lfn):
+    lfn=_lfns[lfn]
+    def ll(x):
+        delta=Y-numpy.matmul(mm.transpose(), x)
+        ll=lfn(delta, w)
+        return -1*ll
+    return ll
 
 def mlft(Y,
          X=None,
          k=None,
          w=None,
          reg=None,
-         lfn="Gauss"):
+         lfn="Gauss",
+         check=True):
     """
     Maximum likelihood Fourier Transform estimator
     """
-    if X is None: X=numpy.linspace(0, 2*numpy.pi, Y.shape[-1])
+    N=Y.shape[-1]
+    if X is None: X=numpy.linspace(0, 2*numpy.pi, N)
     if k is None: k=numpy.arange(0, X.size/2)
     if w is None: w=numpy.ones_like(X)
     cm=numpy.cos(numpy.outer(k, X) )
     sm=numpy.sin(numpy.outer(k, X) )
     mm=numpy.vstack([cm, sm])
-    def ll(x):
-        delta=Y-numpy.matmul(mm.transpose(), x)
-        delta=delta*w
-        ll=_lfns[lfn](delta, w)
-        return -1*ll
+    ll=jit(mkll(Y, mm, w, lfn))
     jac=jit(jacrev(ll))
-    r=scipy.optimize.minimize(ll, numpy.zeros_like(X))
-    return r
+    r=scipy.optimize.minimize(ll,
+                              numpy.zeros_like(X),
+                              jac=jac,
+                              method="BFGS",)
+
+    if check and not r.success:
+        print (r)
+        raise RuntimeError("Did not converge")
+    ff=r.x[0:N//2]+1j*r.x[N//2:]
+    return ff
     
 
