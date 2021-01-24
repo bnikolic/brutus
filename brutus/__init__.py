ad32c6c4da5cee502d4da37d655b2abe4aa8431e
Bojan Nikolic <bn204@cam.ac.uk>
2021-01-24 16:44:57 +0000

    Compute by default the negative frequencies too

--- brutus/__init__.py
+++ brutus/__init__.py
@@ -27,10 +27,13 @@ def mlft(Y,
          check=True):
     """
     Maximum likelihood Fourier Transform estimator
+    
+    :param Y: Data to be fourier transformed 
     """
     N=Y.shape[-1]
     if X is None: X=numpy.linspace(0, 2*numpy.pi, N)
-    if k is None: k=numpy.arange(0, X.size/2)
+    if k is None: k=numpy.hstack([ numpy.arange(0, N//2),
+                                   numpy.arange(-N//2, 0) ])
     if w is None: w=numpy.ones_like(X)
     cm=numpy.cos(numpy.outer(k, X) )
     sm=numpy.sin(numpy.outer(k, X) )
@@ -38,14 +41,14 @@ def mlft(Y,
     ll=jit(mkll(Y, mm, w, lfn))
     jac=jit(jacrev(ll))
     r=scipy.optimize.minimize(ll,
-                              numpy.zeros_like(X),
+                              numpy.zeros(shape=(N*2)),
                               jac=jac,
                               method="BFGS",)
 
     if check and not r.success:
         print (r)
         raise RuntimeError("Did not converge")
-    ff=r.x[0:N//2]+1j*r.x[N//2:]
+    ff=r.x[0:N]-1j*r.x[N:]
     return ff
     
 
